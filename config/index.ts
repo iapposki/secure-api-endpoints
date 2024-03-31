b8e7eead9ad5864afc0507df66dac4e8ffd80c3e
iapposki <konark981999@gmail.com>
2024-03-31 22:50:43 +0530

    configured public api url

--- config/index.ts
+++ config/index.ts
@@ -4,4 +4,5 @@ dotenv.config();
 export const config = {
   authSecret: process.env["AUTH_SECRET"] || "secret",
   serverAddress: process.env["SERVER_ADDRESS"] || "http://localhost:5000/",
+  apiUrl: "https://api.publicapis.org/",
 };
