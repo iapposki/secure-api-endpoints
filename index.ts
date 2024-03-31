a349819c7adde641e36b1976fbda3ac59f1b4cac
iapposki <konark981999@gmail.com>
2024-04-01 04:24:16 +0530

    optimized app

--- index.ts
+++ index.ts
@@ -3,9 +3,8 @@ import express from 'express'
 import { Request, Response, Express } from 'express';
 import * as bodyParser from 'body-parser';
 import cors from 'cors'
-import { signUp, login } from './controllers/auth.controller';
-import { authenticate } from './middlewares/auth.middleware';
-import { getEntries } from './controllers/apiData.controller';
+import swaggerDocs from './utils/swagger.utils';
+import { routes } from './routes';
 
 
 const app: Express = express();
@@ -23,19 +22,13 @@ app.get('/status', async (req: Request, res: Response) => {
     res.send("Node server is running")
 })
 
-// -------------------------------------------------------
-
-
-
-app.post('/auth/signup', signUp)
-app.post('/auth/login', login)
-
-app.get('/entries', authenticate, getEntries)
-
 // -----------------------------------------------------
+
 if (require.main === module) {
     app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
+        routes(app);
+        swaggerDocs(app, port);
     })
 }
 
