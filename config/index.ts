9cf527bf8ea349a5b35333504ffc11d5437bee4e
iapposki <konark981999@gmail.com>
2024-04-02 19:48:07 +0530

    added web3 provider address

--- config/index.ts
+++ config/index.ts
@@ -5,4 +5,5 @@ export const config = {
   authSecret: process.env["AUTH_SECRET"] || "secret",
   serverAddress: process.env["SERVER_ADDRESS"] || "http://localhost:5000/",
   apiUrl: "https://api.publicapis.org/",
+  web3ProviderAddress: process.env["WEB3_PROVIDER_ADDRESS"] || ""
 };
